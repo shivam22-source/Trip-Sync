364e3c6a9291cadf1ddba24a81f361faad81c6c8
b424056@iiit-bh.ac.in <b424056@iiit-bh.ac.in>
2026-05-24 17:28:33 +0530

    .

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "https://trip-sync-smoky.vercel.app",
+  "https://trip-sync-shivam-thakurs-projects-99915067.vercel.app",
 ];
 
 module.exports = {
